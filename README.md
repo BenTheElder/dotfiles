e721ed2b965c95b2ad8e6a5ebb0ab2300db3922c
BenTheElder <bentheelder@gmail.com>
2017-02-17 01:55:28 -0500

    new bootstrap script, custom prompt, vscode settings in OS dependent location

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 This repository contains utilites and common config files for setting up
 my development machines.
+
+# setup
+
+`/tilde/` contains the dotfiles that will go in `~/`  
+`/tilde_macos/` contains dotfiles for macOS only.  
+`/tiled_linux/` contains dotfiles for Linux only.  
+
+`bootstrap.sh` will symlink the files.
---
+++ bootstrap.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# bootstrap dotfiles onto a machine
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#######################################
+# Return if line is in file
+# Globals:
+#   LINE
+#   FILE
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+line_present()
+{
+  grep -qsFx "${LINE}" ${FILE}
+}
+
+#######################################
+# Sets up .bash_profile .bashrc to import common settings
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+bootstrap_bash()
+{
+  common_bash_inc="${DIR}/common.bash.inc"
+  # canary line
+  LINE="if [ -f ${common_bash_inc} ]; then"
+  # files to check
+  bash_files[0]=${HOME}"/.bash_profile"
+  bash_files[1]=${HOME}"/.bashrc"
+  # check all files and add the lines
+  for bash_file in ${bash_files[@]}; do
+    # ensure file exists
+    if [[ -nf ${bash_file} ]]; then
+      touch ${bash_file}
+    fi
+    # ensure common 
+    FILE=${bash_file}
+    line_present
+    if [[ "$?" -ne "0" ]]; then
+      echo "Adding to: \`${FILE}\`."
+      (
+        set -x;
+        echo "" >> ${FILE};
+        echo "# source common settings from dotfiles" >> ${FILE};
+        echo ${LINE} >> ${FILE};
+        echo "  source ${common_bash_inc}" >> ${FILE};
+        echo "fi" >> ${FILE};
+        echo "" >> ${FILE};
+      )
+    fi
+  done
+  # source in current terminal
+  source ${DIR}/common.bash.inc
+}
+
+#######################################
+# Symlinks files in LINK_DIR to ~/
+# Globals:
+#   LINK_DIR
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+symlink_dir()
+{
+  for link_file in $(find "${LINK_DIR}" -type f); do
+    link_target="${HOME}"${link_file#$LINK_DIR}
+    (
+      set -x;
+      ln -s ${link_file} ${link_target};
+    )
+  done
+}
+
+# entry point
+main()
+{
+  echo "DIR := ${DIR}";
+  echo "";
+  read -p "Add \`common.bash.inc\`? (y/n): " -n 1 reply;
+  echo "";
+  if [[ ${reply} =~ ^[Y|y]$ ]]; then
+    bootstrap_bash
+  fi
+  read -p "Symlink files in ./tilde to ~/ ? (y/n): " -n 1 reply;
+  echo "";
+  if [[ ${reply} =~ ^[Y|y]$ ]]; then
+    LINK_DIR="${DIR}/tilde"
+    symlink_dir
+  fi
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo -e "\nDetected macOS";
+    read -p "Symlink files in ./tilde_macos to ~/ ? (y/n): " -n 1 reply;
+    if [[ ${reply} =~ ^[Y|y]$ ]]; then
+      LINK_DIR="${DIR}/tilde_macos"
+      symlink_dir
+    fi
+  elif [[ "$OSTYPE"  == "linux-gnu" ]]; then
+    echo "Detected Linux";
+    read -p "Symlink files in ./tilde_linux to ~/ ? (y/n): " -n 1 reply;
+    if [[ ${reply} =~ ^[Y|y]$ ]]; then
+      LINK_DIR="${DIR}/tilde_linux"
+      symlink_dir
+    fi
+  fi
+  echo "";
+}
+
+main "$@"
