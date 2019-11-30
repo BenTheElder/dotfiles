c9ecc2d385f38392311cc2e922432d4cf96cc1ce
Benjamin Elder <ben@elder.dev>
2019-11-30 11:49:59 -0800

    fix typo

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -10,7 +10,7 @@ run_script_traced() {
 # this scipt bootstraps my typical debian server setup
 set -x;
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-SCRIPTS="${SCRIPTS_DIR}/setup"
+SCRIPTS="${SCRIPT_DIR}/setup"
 run_script_traced "${SCRIPTS}"/install-common.sh
 run_script_traced "${SCRIPTS}"/automatic-upgrades.sh
 run_script_traced "${SCRIPTS}"/disable-sleep.sh
