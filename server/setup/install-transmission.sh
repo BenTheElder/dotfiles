e4f1bb09808fe48b3627cd21d43d3468aa046fc4
Benjamin Elder <ben@elder.dev>
2019-11-30 18:49:42 -0800

    fix sed again

--- server/setup/install-transmission.sh
+++ server/setup/install-transmission.sh
@@ -22,14 +22,14 @@ User=${ORIGINAL_USER}
 EOF
 
 # only bind the web ui to localhost
-sed -i "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json 's#"rpc-bind-address":.*#"rpc-bind-address": "127.0.0.1",#'
+sed -i 's#"rpc-bind-address":.*#"rpc-bind-address": "127.0.0.1",#' "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json
 
 # disable dht and pex, we'll be using our own trackers / peers
-sed -i "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json 's#"dht-enabled":.*#"dht-enabled": false,#'
-sed -i "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json 's#"pex-enabled":.*#"pex-enabled": false,#'
+sed -i 's#"dht-enabled":.*#"dht-enabled": false,#' "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json
+sed -i 's#"pex-enabled":.*#"pex-enabled": false,#' "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json
 
 # set download dir
-sed -i "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json 's#"download-dir":.*#"download-dir": "'"${TRANSMISSION_DOWNLOAD_DIR}"'",#'
+sed -i 's#"download-dir":.*#"download-dir": "'"${TRANSMISSION_DOWNLOAD_DIR}"'",#' "${ORIGINAL_HOME}"/.config/transmission-daemon/settings.json
 
 # restart transmission with latest settings
 systemctl daemon-reload
