08c603c7578642de09ab88ccd39b39ccaf3f4641
Benjamin Elder <ben@elder.dev>
2019-12-23 03:30:55 -0800

    add title to ydlep.sh

--- utils/ydlep.sh
+++ utils/ydlep.sh
@@ -6,7 +6,8 @@ set -x;
 # zero pad season / episode
 SEASON="$(printf '%02d' "${SEASON:?}")"
 EPISODE="$(printf '%02d' "${EPISODE:?}")"
+TITLE="${TITLE:+" - ${TITLE}"}"
 BASEDIR="${BASEDIR:-"/mnt/storage/plex/tv"}"
 
 # call ydl.sh with download location based on $SHOW, $SEASON, $EPISODE
-"${SCRIPT_DIR}/ydl.sh" "--output=${BASEDIR}/${SHOW:?}/Season ${SEASON:?}/${SHOW:?} - s${SEASON:?}e${EPISODE:?}.%(ext)s" "$@"
+"${SCRIPT_DIR}/ydl.sh" "--output=${BASEDIR}/${SHOW:?}/Season ${SEASON:?}/${SHOW:?} - s${SEASON:?}e${EPISODE:?}${TITLE}.%(ext)s" "$@"
