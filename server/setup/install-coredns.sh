990362e18f48f158ffb06557bb2a48c51143758b
Benjamin Elder <ben@elder.dev>
2022-02-05 23:13:07 -0800

    add coredns install script

---
+++ server/setup/install-coredns.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# this scipt installs https://coredns.io/
+
+version='1.8.7'
+
+arch='amd64'
+base_url="https://github.com/coredns/coredns/releases/download/v${version}"
+tarball="coredns_${version}_linux_${arch}.tgz"
+tarball_url="${base_url}/${tarball}"
+
+# create tempdir to perform downloads in
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "${tmpdir}"' EXIT
+
+# download tarball and hash
+curl -o "${tmpdir}/${tarball}" "${tarball_url}"
+curl -o "${tmpdir}/${tarball}.sha256" "${tarball_url}.sha256"
+
+# check hash
+sha256sum --check --status "${tmpdir}/${tarball}.sha256"
+
+# extract and install
+tar -C /usr/local/bin -xzvf "${tmpdir}/${tarball}"
