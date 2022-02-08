61d0f8cb73a36a4550852009ae0347edd8619389
Benjamin Elder <ben@elder.dev>
2022-02-07 23:27:14 -0800

    refactor hosts update, inject server hostname

--- server/setup/configure-coredns.sh
+++ server/setup/configure-coredns.sh
@@ -21,7 +21,7 @@ touch "${block_hosts_path}"
 cat <<EOF >"${corefile_path}"
 .:53 {
     hosts /etc/coredns/hosts {
-        reload 3600s
+        reload 5s
         fallthrough
     }
     forward . tls://9.9.9.9 tls://149.112.112.112 tls://2620:fe::fe tls://2620:fe::9 {
@@ -66,13 +66,33 @@ EOF
 # add service to periodically fetch hosts
 # see: https://github.com/StevenBlack/hosts
 # adware + malware => 0.0.0.0
+readonly update_hosts_script='/etc/coredns/update-hosts.sh'
+cat <<EOF >"${update_hosts_script}"
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# we expect to have a static IP but still let's detect this each time
+host_name="\$(hostname)"
+host_default_ip="\$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')"
+
+# fetch hosts file
+hosts_path='${block_hosts_path}'
+curl -o "\${hosts_path}" -L https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+
+# post-process
+echo '${host_default_ip} "\${host_name}.local." >>"\${hosts_path}"
+EOF
+chmod +x "${update_hosts_script}"
+
 cat <<EOF >/etc/systemd/system/update-coredns-hosts.service
 [Unit]
 Description=Updates CoreDNS hosts file
 
 [Service]
 Type=simple
-ExecStart=curl -o "${block_hosts_path}" -L https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+ExecStart=${update_hosts_script}
 
 [Install]
 WantedBy=default.target
