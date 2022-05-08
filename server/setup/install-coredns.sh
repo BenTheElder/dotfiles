2f49358d947b3bd99613be74e15d910d17775969
Benjamin Elder <ben@elder.dev>
2022-05-07 23:38:13 -0700

    update coredns to v1.9.1

--- server/setup/install-coredns.sh
+++ server/setup/install-coredns.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 # this scipt installs https://coredns.io/
 
-version='1.8.7'
+version='1.9.1'
 if which coredns >/dev/null; then
     current_version="$(coredns --version | head -n1 | sed -nr 's#CoreDNS-(.*)#\1#p')"
     if [[ "${current_version}" = "${version}" ]]; then
@@ -16,6 +16,7 @@ fi
 
 >&2 echo "Installing CoreDNS v${version}"
 
+# TODO: detect arch
 arch='amd64'
 base_url="https://github.com/coredns/coredns/releases/download/v${version}"
 tarball="coredns_${version}_linux_${arch}.tgz"
@@ -32,6 +33,7 @@ curl -o "${tmpdir}/${tarball}.sha256" -L "${tarball_url}.sha256"
 cat "${tmpdir}/${tarball}.sha256"
 
 # check hash
+# TODO: this approach only gives us an integrity check
 (cd "${tmpdir}" && sha256sum --check --status "${tmpdir}/${tarball}.sha256")
 
 # extract and install
